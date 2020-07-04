07f89a3769938303ef6c70bebe8b5677aededb8f
mgsr-ifnmg <67842894+mgsr-ifnmg@users.noreply.github.com>
2020-07-04 18:42:39 -0300

    Update hello_world.asm

--- hello_world.asm
+++ hello_world.asm
@@ -8,12 +8,12 @@ msg:						; address to store a block of data
 section .text					; section of text -> instructions
 
 global _start					; global _start label
-_start:							; the execution start here
+_start:						; the execution start here
 	mov rax, 0x1				; write 1 into rax -> the id of write syscall (linux)
 	mov rdi, 0x1				; write 1 into rdi -> STDOUT file descriptor
 	mov rsi, msg				; write the address of msg into tsi
 	mov rdx, 0xe				; write the length of message into rdx - 14 bytes
-	syscall						; call write(rdi, rsi, rdx)
+	syscall					; call write(rdi, rsi, rdx)
 
 	mov rax, 0x3c				; write 60 into rax -> the id of exit syscall (linux)
 	xor rdi, rdi				; perform a xor bit-to-bit operation into rdi with itself
